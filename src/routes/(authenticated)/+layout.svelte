e76ccc0dda17cc4df9eec46f5b8f557a3c79c290
EdisonP <133362311+edions@users.noreply.github.com>
2024-07-12 19:17:30 +0800

    add product table

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -144,8 +144,6 @@
 				</DropdownMenu.Content>
 			</DropdownMenu.Root>
 		</header>
-		<main class="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-			<slot/>
-		</main>
+		<slot/>
 	</div>
 </div>
